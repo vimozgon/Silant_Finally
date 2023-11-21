660078773accea83efa682a5b39b0bf7da9b88ea
vimozgon <107113351+vimozgon@users.noreply.github.com>
2023-11-21 21:35:06 +0300

    Finnaly

---
+++ silant/adapter.py
@@ -0,0 +1,8 @@
+from allauth.account.adapter import DefaultAccountAdapter
+
+
+
+class NoNewUsersAccountAdapter(DefaultAccountAdapter):
+
+    def is_open_for_signup(self, request):
+        return False
